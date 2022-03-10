9be92ee9a2865cd0c798fd175f121d621d03a254
Eloisa <27989510+eefborromeo@users.noreply.github.com>
2022-03-10 22:24:29 +0800

    created All Users page

--- src/pages/NewUser.js
+++ src/pages/NewUser.js
@@ -1,12 +1,78 @@
 import React from "react";
+import styled from 'styled-components';
+
+const Container  = styled.main`
+    display: flex;
+    height: 100vh;
+`;
+
+const SideBar = styled.div`
+    background: #222;
+    border-right: 2px solid rgb(236, 236, 236);
+    width: 15vw;
+    padding: 2rem;
+    color: #fff;
+    font-weight: bold;
+    font-size: 1.5rem;
+    ul {
+        list-style-type: none;
+        padding: 0;
+        font-size: 1rem;
+        li {
+            padding: 1rem 1rem 1rem 0;
+        }
+    }
+`;
+
+const Content = styled.div`
+    background-color: rgb(236, 236, 236);
+    flex: 1;
+    padding: 2rem;
+
+    form {
+        background-color: #fff;
+        padding: 2rem;
+        border-radius: 15px;
+        width: 80%;
+
+        input {
+            border: none;
+            border-bottom: 2px solid rgb(236, 236, 236);
+            width: 100%;
+        }
+
+        input:nth-child(2) {
+            margin-bottom: 1rem;
+        }
+
+        button {
+            display: block;
+            margin-top: 1rem;
+            margin-left: auto;
+            border: none;
+            padding: .8rem 1.8rem;
+            background-color: #222;
+            color: white;
+            border-radius: 5px;
+            font-weight: bold;
+        }
+    }
+`;
 
 export default function NewUser() {
     return (
-        <main>
-            <div className="sidebar">
-
-            </div>
-            <div className="content">
+        <Container>
+            <SideBar>
+                <div>
+                    LOGO
+                </div>
+                <ul>
+                    <li>Create User</li>
+                    <li>All Users</li>
+                </ul>
+            </SideBar>
+            <Content>
+                <h1>Create New User</h1>
                 <form>
                     <div>
                         <p>Name</p>
@@ -16,10 +82,10 @@ export default function NewUser() {
                         <p>Initial Balance</p>
                         <input type="number" />
                     </div>
-                    <button type="submit" />
+                    <button type="submit">Submit</button>
                 </form>
-            </div>
-        </main>
+            </Content>
+        </Container>
     )
 }
 
