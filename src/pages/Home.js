5432fd2593bfae7c5be2bbfd50566c6c6a83bf74
Eloisa <27989510+eefborromeo@users.noreply.github.com>
2022-03-15 08:12:31 +0800

    created landing page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,123 @@
 import React from "react";
+import styled from "styled-components"
+import logo from "../images/logo.png"
+import phone from "../images/phone.png"
+
+const Background = styled.div`
+  background-color: #596dc4;
+  height: 100vh;
+  * {
+    box-sizing: border-box;
+  }
+`
+
+const LogoContainer = styled.div`
+  width: 15%;
+  padding: 2rem;
+  display: block;
+  > img {
+    width: 100%;
+  }
+
+  @media (max-width: 768px) {
+    width: 40%;
+  }
+`
+
+const FlexContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 90%;
+  margin-left: auto;
+  > div {
+    color: #fff;
+    div {
+      margin-top: 2rem;
+    }
+  }
+  h1 {
+    font-size: 5rem;
+    margin-bottom: 1rem;
+  }
+  p {
+    font-size: 2rem;
+    margin-bottom: 1rem;
+  }
+
+  input {
+    border: none;
+    width: 60%;
+    padding: 1rem;
+    border-radius: 30px;
+  }
+
+  button {
+    border: none;
+    margin-left: 20px;
+    background-color: #415195;
+    padding: 1rem 2rem;
+    border-radius: 30px;
+    color: #fff;
+    font-weight: 600;
+  }
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    width: 70%;
+    margin: auto;
+    padding-bottom: 2rem;
+    justify-content: center;
+    h1 {
+      font-size: 2.5rem;
+    }
+    p {
+      font-size: 1.5rem;
+    }
+    input {
+      width: 40%;
+    }
+    button {
+    margin-left: 5px;
+
+    }
+  }
+}
+`
+
+const ImageContainer = styled.div`
+  width: 80%;
+  img {
+    width: 100%;
+  }
+  @media (max-width: 768px) {
+    width: 100%;
+    order: -1;
+  }
+`
 
 export default function Home() {
-  return <div>This is Home</div>;
+  return (
+    <Background>
+      <LogoContainer>
+        <img src={logo} alt="Cashpoint Logo"/>
+      </LogoContainer>
+      <FlexContainer>
+        <div>
+          <h1>Coming Soon!</h1>
+          <p>Our team is actively working on this project. See you around!</p>
+          <div>
+            <p>Sign up to get early access!</p>
+            <form>
+              <input type="text"/>
+              <button>Add me!</button>
+            </form>
+          </div>
+        </div>
+        <ImageContainer>
+          <img src={phone} alt="Cashpoint App" />
+        </ImageContainer>
+      </FlexContainer>
+    </Background>
+  );
 }
