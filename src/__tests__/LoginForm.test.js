28c44c2ede4905d4c3a6055ae57278f58c3b598b
Spike Vinz <spikevinz@gmail.com>
2022-03-21 18:22:35 +0800

    setup mocks for zustand
    add unit test for admin LogIn component

---
+++ src/__tests__/LoginForm.test.js
@@ -0,0 +1,32 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import { renderHook } from "@testing-library/react-hooks";
+import LoginForm from "../components/LoginForm";
+import useStore from "../store";
+
+it("should login the admin if the credentials are correct", () => {
+  const { result } = renderHook(() => useStore((state) => state));
+  render(<LoginForm />);
+
+  const usernameInput = screen.getByLabelText(/Username/);
+  const passwordInput = screen.getByLabelText(/Password/);
+
+  fireEvent.change(usernameInput, { target: { value: "admin" } });
+  fireEvent.change(passwordInput, { target: { value: "admin" } });
+  fireEvent.click(screen.getByText("Login"));
+
+  expect(result.current.loggedIn).toBe(true);
+});
+
+it("should not login the admin if the credentials are incorrect", () => {
+  const { result } = renderHook(() => useStore((state) => state));
+  render(<LoginForm />);
+
+  const usernameInput = screen.getByLabelText(/Username/);
+  const passwordInput = screen.getByLabelText(/Password/);
+
+  fireEvent.change(usernameInput, { target: { value: "admin" } });
+  fireEvent.change(passwordInput, { target: { value: "asdf" } });
+  fireEvent.click(screen.getByText("Login"));
+
+  expect(result.current.loggedIn).toBe(false);
+});
